7af2daf6d0c271f646a0879bc464a084b8c8a77c
Arturo Duarte <marturo77@hotmail.com>
2024-10-13 09:54:04 -0500

    Ajustes

--- Extensions.cs
+++ Extensions.cs
@@ -3,7 +3,7 @@
 namespace BuildF394
 {
     /// <summary>
-    ///
+    /// Metodos de extension
     /// </summary>
     public static class Extensions
     {
@@ -19,37 +19,78 @@ namespace BuildF394
         }
 
         /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="pad"></param>
+        /// <returns></returns>
+        public static string PadZerosLeft(this double value, int pad = 8)
+        {
+            value = Math.Round(value, 2);
+            return value.ToString("0.00").Replace(",", ".").PadLeft(pad, '0');
+        }
+
+        /// <summary>
         /// Funciones para identificar tipo de dato
         /// </summary>
-        /// <param name="columna"></param>
+        /// <param name="index"></param>
         /// <returns></returns>
-        public static bool EsTexto(this int columna) => new[] { 8, 10, 21, 47, 55, 63, 66, 76 }.Contains(columna);
+        public static bool IsText(this int index) => new[] { 20, 46, 54, 62, 65, 75 }.Contains(index);
+
+        /// <summary>
+        /// /
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        // Es la columna del codigo poliza
+        public static bool IsInsuranceCode(this int index) => new[] { 7, 9 }.Contains(index);
 
         /// <summary>
         ///
         /// </summary>
-        /// <param name="columna"></param>
+        /// <param name="index"></param>
         /// <returns></returns>
-        public static bool EsFecha(this int columna) => new[] { 9, 11, 20, 46, 54, 62, 83 }.Contains(columna);
+        public static bool IsRamo(this int index) => new[] { 0 }.Contains(index);
 
         /// <summary>
         ///
         /// </summary>
-        /// <param name="columna"></param>
+        /// <param name="index"></param>
         /// <returns></returns>
-        public static bool EsNumero(this int columna) => new[] { 4, 6, 7, 30, 32, 37, 79, 80, 81, 82 }.Contains(columna);
+        public static bool IsDate(this int index) => new[] { 8, 10, 19, 45, 53, 61, 82 }.Contains(index);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static bool IsNumber(this int index) => new[] { 3, 5, 6, 29, 31, 36, 78, 79, 80, 81 }.Contains(index);
+
+        /// <summary>
+        /// Es una columna para consolidar
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static bool IsConsolidation(this int index) => ConsolidationIndex().Contains(index);
+
+        /// <summary>
+        /// Indices de columnas que hay que consolidar o sumar
+        /// </summary>
+        /// <returns></returns>
+        public static int[] ConsolidationIndex() => new[] { 26, 32, 33, 34, 37, 76, 77, 83 }.ToArray();
 
         /// <summary>
         /// Función para verificar si la celda no está vacía
         /// </summary>
         /// <param name="sheet"></param>
-        /// <param name="fila"></param>
-        /// <param name="columna"></param>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
         /// <returns></returns>
-        public static bool CeldaNoVacia(this ISheet sheet, int fila, int columna)
+        public static bool IsNotEmpty(this ISheet sheet, int row, int col)
         {
-            return sheet.GetRow(5 + fila)?.GetCell(columna - 1) != null &&
-                   sheet.GetRow(5 + fila).GetCell(columna - 1).ToString() != "";
+            object? value = sheet.GetRow(Program.ROW_OFFSET + row)?.GetCell(col);
+
+            return value != null && value.ToString() != "";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using NPOI.SS.UserModel;
+﻿using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
 namespace BuildF394
@@ -11,7 +12,17 @@ namespace BuildF394
         /// <summary>
         /// Offset en las filas para la pagina 1
         /// </summary>
-        private const int ROW_OFFSET = 5;
+        public const int ROW_OFFSET = 6;
+
+        /// <summary>
+        /// Total de columnas en la pagina 1
+        /// </summary>
+        public const int TOTAL_COLUMNS = 84;
+
+        /// <summary>
+        /// Numero de filas que tendra el archivo plano en el encabezado
+        /// </summary>
+        public const int HEADER_ROWS = 3;
 
         /// <summary>
         ///
@@ -72,12 +83,11 @@ namespace BuildF394
                     // Obtener las hojas de cálculo
                     ISheet hoja = workbook.GetSheetAt(0);
 
-                    Console.WriteLine("ObtenerRegistros");
-
                     Console.WriteLine("ProcesarFilas");
                     List<string[]> datos = ProcesarFilas(hoja, ContarRegistros(hoja));
 
-                    // Fecha del reporte
+                    // Resolver Fecha del reporte
+                    // TODO: Revisar el tema del mes que presento un problema en el parsing
                     string date = hoja.GetRow(0).GetCell(5).ToString();
                     string day = date.Substring(0, 2);
                     string month = "12";//date.Substring(3, 3).ToUpper();
@@ -86,30 +96,44 @@ namespace BuildF394
                     Console.WriteLine("Escribiendo encabezados");
 
                     // Entidad con los datos del encabezado
-                    HeaderData d = new HeaderData();
+                    HeaderData headerData = new HeaderData();
 
-                    //REGISTRO TIPO 1
+                    #region Registro Tipo 1
+
                     //la secuencia es 1;debe tener 48 caracteres
-                    string tipo1 = $"{1.PadZerosLeft()}{1}{d.EntityType}{d.EntityCode}{day}{month}{year}{datos.Count().PadZerosLeft()}{d.KeyWord}{d.Area}{d.ReportType}";
+                    string tipo1 = $"{1.PadZerosLeft()}{1}{headerData.EntityType}{headerData.EntityCode}{day}{month}{year}{datos.Count().PadZerosLeft()}{headerData.KeyWord}{headerData.Area}{headerData.ReportType}";
                     if (tipo1.Length != 48) throw new Exception("Registro tipo 1 invalido");
 
-                    // REGISTRO TIPO 3
+                    #endregion Registro Tipo 1
+
+                    #region RegistroTipo 2
+
                     // la secuencia es 2; debe tener 43 caracteres
                     string evaluationType = "0";
                     int fideicomiso = 0;
                     string tipo3 = $"{2.PadZerosLeft()}{3}{evaluationType}{fideicomiso.PadZerosLeft(17)}0000000000000000";
                     if (tipo3.Length != 43) throw new Exception("Registro tipo 3 invalido");
 
-                    // REGISTRO TIPO 4
+                    #endregion RegistroTipo 2
+
+                    #region Registro Tipo 4
+
                     //la secuencia es 3; debe tener 31 caracteres
                     string tipo4 = $"{3.PadZerosLeft()}{4}0000000000000000000002";
                     if (tipo4.Length != 31) throw new Exception("Registro tipo 4 invalido");
 
-                    // REGISTRO TIPO 6
+                    #endregion Registro Tipo 4
+
+                    #region Registro tipo 6
+
                     //la secuencia va de ultimo; debe tener 31 caracteres
                     int lastSequence = datos.Count() + 3;
                     string tipo6 = $"{lastSequence.PadZerosLeft()}{6}";
                     if (tipo6.Length != 9) throw new Exception("Registro tipo 6 invalido");
+
+                    #endregion Registro tipo 6
+
+                    #region Colocar registros al principio y el tipo6 al final
 
                     // tipo1, tipo3, tipo 4 van al principio
                     datos.Insert(0, new string[] { tipo1 });
@@ -119,8 +143,14 @@ namespace BuildF394
                     // Registro de cierre al final
                     datos.Add(new string[] { tipo6 });
 
-                    Console.WriteLine("EscribirEnArchivoPlano");
-                    EscribirEnArchivoPlano(datos, month, year, rutaEjecutable);
+                    #endregion Colocar registros al principio y el tipo6 al final
+
+                    #region Escribir en disco
+
+                    Console.WriteLine("Escribir Archivo Plano");
+                    WriteTextPlain(datos, month, year, rutaEjecutable);
+
+                    #endregion Escribir en disco
 
                     Console.WriteLine("Proceso completado.");
                 }
@@ -138,7 +168,7 @@ namespace BuildF394
         /// <param name="mes"></param>
         /// <param name="año"></param>
         /// <param name="rutaEjecutable"></param>
-        private static void EscribirEnArchivoPlano(List<string[]> datos, string mes, string año, string rutaEjecutable)
+        private static void WriteTextPlain(List<string[]> datos, string mes, string año, string rutaEjecutable)
         {
             // Construir nombre del archivo de salida
             string nombre = $"{mes}-{año}-fto394.txt";
@@ -148,8 +178,6 @@ namespace BuildF394
             // Borra el archivo si existe
             if (File.Exists(rutaGuardado)) File.Delete(rutaGuardado);
 
-            Console.WriteLine($"Archivo creado {rutaGuardado}");
-
             using (StreamWriter writer = new StreamWriter(rutaGuardado))
             {
                 foreach (var fila in datos)
@@ -158,102 +186,179 @@ namespace BuildF394
                     writer.WriteLine(string.Join("", fila));
                 }
             }
+
+            Console.WriteLine($"Archivo creado {rutaGuardado}");
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="secuence"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        private static string[] GetConsolidationRecord(int secuence, int col, double total)
+        {
+            // Almacenar todos los datos en un array de strings
+            string[] record = new string[8];
+
+            //Secuencia 8 caracteres
+            record[0] = secuence.PadZerosLeft();
+
+            //Tipo de registro 5
+            record[1] = "5";
+
+            //Codigo de formato
+            record[2] = "394";
+
+            //Codigo de Columna
+            record[3] = (col + 1).PadZerosLeft(2);
+
+            //Codigo de unidad de captura
+            record[4] = "02";
+
+            //Codigo de la subcuenta
+            record[5] = "000001";
+
+            //Signo
+            record[6] = "+";
+
+            //Valor longitud 17
+            record[7] = total.PadZerosLeft(17);
+
+            return record;
         }
 
         /// <summary>
         /// Crea registros tipo 5
         /// </summary>
-        /// <param name="secuencia"></param>
-        /// <param name="fila"></param>
-        /// <param name="columna"></param>
-        /// <param name="hoja"></param>
+        /// <param name="sheet"></param>
+        /// <param name="secuence"></param>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
         /// <returns></returns>
-        private static string[] CrearFila(int secuencia, int fila, int columna, ISheet hoja)
+        private static string[] GetRecord(ISheet sheet, int secuence, int row, int col)
         {
-            string nceros6 = new string('0', 6 - fila.ToString().Length);
-
             // Almacenar todos los datos en un array de strings
-            string[] result = new string[8];
+            string[] record = new string[8];
 
-            //Secuencia 8
-            result[0] = secuencia.PadZerosLeft();
+            //Secuencia 8 caracteres
+            record[0] = secuence.PadZerosLeft();
 
             //Tipo de registro 5
-            result[1] = "5";
+            record[1] = "5";
 
             //Codigo de formato
-            result[2] = "394";
+            record[2] = "394";
 
             //Codigo de Columna
-            result[3] = columna.PadZerosLeft(2);
+            record[3] = (col + 1).PadZerosLeft(2);
 
-            // Codigo de unidad de captura
-            result[4] = "01";
+            //Codigo de unidad de captura
+            record[4] = "01";
 
-            // Codigo de la subcuenta
-            result[5] = nceros6 + fila;
+            //Codigo de la subcuenta
+            record[5] = (row + 1).PadZerosLeft(6);
 
             //Signo
-            result[6] = "+";
+            record[6] = "+";
 
-            //Valor longitud 17 o 50
-            result[7] = ObtenerValorCelda(hoja, fila, columna);
+            //Valor longitud 17
+            record[7] = GetValue(sheet, row, col);
 
-            return result;
+            return record;
         }
 
         /// <summary>
         ///
         /// </summary>
-        /// <param name="hoja"></param>
-        /// <param name="fila"></param>
+        /// <param name="sheet"></param>
+        /// <param name="row"></param>
         /// <param name="columna"></param>
         /// <returns></returns>
-        private static string ObtenerValorCelda(ISheet hoja, int fila, int columna)
+        private static string GetValue(ISheet sheet, int row, int col)
         {
-            if (columna.EsTexto())
+            ICell cell = sheet.GetRow(ROW_OFFSET + row).GetCell(col);
+
+            if (col.IsRamo())
             {
-                return hoja.GetRow(ROW_OFFSET + fila).GetCell(columna - 1).ToString().PadRight(50);
+                return cell.ToString().PadLeft(17, '0');
             }
-            else if (columna.EsFecha())
+            else
+                if (col.IsInsuranceCode())
             {
-                DateTime fecha = hoja.GetRow(ROW_OFFSET + fila).GetCell(columna - 1).DateCellValue.Value;
-                string day = fecha.Day.PadZerosLeft(2);
-                string month = fecha.Month.PadZerosLeft(2);
-                string year = fecha.Year.ToString();
-                return $"{day}{month}{year}";
+                return cell.NumericCellValue.PadZerosLeft(17);
             }
-            else if (columna.EsNumero())
+            else if (col.IsText())
             {
-                double valor = hoja.GetRow(ROW_OFFSET + fila).GetCell(columna - 1).NumericCellValue;
-                return Math.Round(valor, 2).ToString("0.00").Replace(",", ".");
+                return cell.ToString().PadRight(50);
             }
-            return "";
+            else if (col.IsDate())
+            {
+                DateTime? date = cell.DateCellValue;
+
+                if (date.HasValue)
+                {
+                    string day = date.Value.Day.PadZerosLeft(2);
+                    string month = date.Value.Month.PadZerosLeft(2);
+                    string year = date.Value.Year.ToString();
+                    return $"{day}{month}{year}";
+                }
+                else throw new Exception($"{row}{col} No tiene un campo de fecha");
+            }
+            else if (col.IsNumber())
+            {
+                return cell.NumericCellValue.PadZerosLeft(17);
+            }
+
+            // Si no devuelve cadena vacha
+            return string.Empty;
         }
 
         /// <summary>
-        ///
+        /// Obtiene un array de registros tipo 5
         /// </summary>
-        /// <param name="hoja"></param>
-        /// <param name="registros"></param>
+        /// <param name="sheet"></param>
+        /// <param name="totalRows"></param>
         /// <returns></returns>
-        private static List<string[]> ProcesarFilas(ISheet hoja, int registros)
+        private static List<string[]> ProcesarFilas(ISheet sheet, int totalRows)
         {
             List<string[]> result = new List<string[]>();
-            int counter = 0;
-            int secuencia = 0;
 
-            for (int col = 1; col <= 84; col++)
+            int secuencia = Program.HEADER_ROWS + 1;
+
+            // Recorre las columnas
+            for (int col = 0; col < Program.TOTAL_COLUMNS; col++)
             {
-                for (int fila = 1; fila <= registros; fila++)
+                double consolidate = 0;
+
+                // Para todas las filas
+                for (int row = 0; row < totalRows; row++)
                 {
-                    if (hoja.CeldaNoVacia(fila, col))
+                    // Si hay datos
+                    if (sheet.IsNotEmpty(row, col))
                     {
-                        counter++;
-                        secuencia = 3 + counter;
-                        string[] filaDatos = CrearFila(secuencia, fila, col, hoja);
-                        result.Add(filaDatos);
+                        // Obtiene un registro tipo 5 para la cuenta
+                        result.Add(GetRecord(sheet, secuencia, row, col));
+
+                        // Si es una columna de consolidacion va sumando
+                        if (col.IsConsolidation())
+                        {
+                            ICell cell = sheet.GetRow(ROW_OFFSET + row).GetCell(col);
+                            consolidate += cell.NumericCellValue;
+                        }
                     }
+
+                    secuencia++;
+                }
+
+                // Es el final de la fila y hay que consolidar
+                if (consolidate > 0)
+                {
+                    Console.WriteLine($"Columna consolidada {col}={consolidate.PadZerosLeft(17)}");
+
+                    // Obtiene un registro tipo 5 para la cuenta
+                    result.Add(GetConsolidationRecord(secuencia, col, consolidate));
                 }
             }
 
@@ -267,13 +372,16 @@ namespace BuildF394
         /// <returns></returns>
         private static int ContarRegistros(ISheet sheet)
         {
-            int j = 1;
-            while (sheet.GetRow(5 + j) != null && sheet.GetRow(ROW_OFFSET + j).GetCell(0) != null &&
-                   sheet.GetRow(5 + j).GetCell(0).CellType != CellType.Blank)
+            int total = 0;
+
+            IRow row = sheet.GetRow(ROW_OFFSET + total);
+
+            while (row != null && row.GetCell(0) != null && row.GetCell(0).CellType != CellType.Blank)
             {
-                j++;
+                total++;
+                row = sheet.GetRow(ROW_OFFSET + total);
             }
-            return j - 1;
+            return total;
         }
     }
 }
